b73b4920754fc5e9400b1295eea18a6516239972
H-S-RATHI <goluu.000111@gmail.com>
2023-09-30 20:27:43 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://H-S-RATHI.github.io/Movie-Search-App-Using-React',
   plugins: [react()],
 })
